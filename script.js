67763d33028b271000671a6ebe200233dd77ff71
Subrata Rout <subrataa916@gmail.com>
2024-01-12 13:55:16 +0530

    final project

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use script';
+const getBtn = document.querySelector('.btn1');
+const copyBtn = document.querySelector('.btn2');
+
+getBtn.addEventListener('click', () => genPassword());
+copyBtn.addEventListener('click', () => copyPassword());
+
+function genPassword() {
+  let chars =
+    '0123456789abcdefghijklmnopqristuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  passwordLength = 11;
+  password = '';
+
+  for (let i = 0; i <= passwordLength; i++) {
+    let randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+  document.getElementById('password').value = password;
+}
+
+function copyPassword() {
+  let copyText = document.getElementById('password');
+  copyText.select();
+  document.execCommand('copy');
+}
